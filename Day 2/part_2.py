6a4afdbc73bbdd6aaa4927fe82ab3df5d95b4b99
Guanyou02 <124653516+Guanyou02@users.noreply.github.com>
2024-12-06 16:37:04 +0800

    Day 2
    learnt sorted vs .sort()

---
+++ Day 2/part_2.py
@@ -0,0 +1,38 @@
+data = open('input.txt', 'r')
+
+number_set = []
+
+for line in data:
+    numbers = line.split()
+    number_set.append([int(num) for num in numbers])  # Convert strings to integers
+data.close()
+
+safe = 0
+
+difference = [1, 2, 3]
+
+for numbers in number_set:
+    all_safe = False
+    if sorted(numbers) == numbers or sorted(numbers, reverse=True) == numbers: # Sorted will not change the original array
+        all_safe = True
+        for i in range((len(numbers)-1))    :
+            if abs(numbers[i+1] - numbers[i]) not in difference:
+                all_safe = False
+                break
+
+    if not all_safe:
+        for i in range(len(numbers)):
+            modified_numbers = numbers[:i] + numbers[i + 1:]
+            if sorted(modified_numbers) == modified_numbers or sorted(modified_numbers, reverse=True) == modified_numbers:
+                all_safe = True
+                for j in range(len(modified_numbers) - 1):
+                    if abs(modified_numbers[j + 1] - modified_numbers[j]) not in difference:
+                        all_safe = False
+                        break
+                if all_safe:
+                    break
+
+    if all_safe:
+        safe += 1
+
+print(safe)
